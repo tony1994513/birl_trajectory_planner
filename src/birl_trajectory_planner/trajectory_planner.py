f1fadb2b98aa9bf3d14fe5d08f5221b007461c9e
Tony <470963660@qq.com>
2019-02-28 16:03:23 +0800

    2-28

--- src/birl_trajectory_planner/trajectory_planner.py
+++ src/birl_trajectory_planner/trajectory_planner.py
@@ -2,30 +2,28 @@
 import os
 import rospy
 import numpy as np
-from birl_trajectory_excution import joint_action_client
 from dmp_util import dmp_generalization
 from birl_trajectory_excution.utils import get_current_angle
-from birl_trajectory_excution._constant import limb
 from birl_inverse_kinematic import trac_ik_solver
 
 dir_of_this_script = os.path.dirname(os.path.realpath(__file__))
 demonstration_model_dir = os.path.join(dir_of_this_script, '..','..', 'dmp_data', 'processed_demonstrations','demos')
 
 
-def planner(start=None, end=None, planner_type = "dmp",phase=None, orig_demo_mode= None):
+def planner(start=None, end=None,limb=None, planner_type = "dmp",phase=None, orig_demo_mode= None,point_mode=None):
 
     if planner_type == "cart_trajectory_action_server":
-        joint = trac_ik_solver.inverse_kinematic(end,one_point_mode=True)
+        joint = trac_ik_solver.inverse_kinematic(end,point_mode=point_mode,limb=limb)
         return joint
     elif planner_type == "dmp":
-        joint_plan = dmp_plan(start,end,phase,orig_demo_mode)
-        return joint_plan
+        joint_plan_list = dmp_plan(start,end,phase,limb=limb,orig_demo_mode=orig_demo_mode)
+        return joint_plan_list
 
     elif planner_type == "promp":
         pass
 
 
-def dmp_plan(start=None, end=None,phase=None, orig_demo_mode= None):
+def dmp_plan(start=None, end=None,limb=None,phase=None, orig_demo_mode= None):
     if phase == 1:
         rospy.loginfo("Dmp planner Phase: %s " %phase)
         demo = np.load(open(os.path.join(demonstration_model_dir,'home_to_prepick', '1.npy'), 'r'))
@@ -52,7 +50,7 @@ def dmp_plan(start=None, end=None,phase=None, orig_demo_mode= None):
         rospy.loginfo("dmp orig end pose is %s\n" %end[0:3] )
     cart_trajectory_plan = dmp_generalization(start, end, demo)
 
-    joint_plan = trac_ik_solver.inverse_kinematic(cart_trajectory_plan)
+    joint_plan_list = trac_ik_solver.inverse_kinematic(cart_trajectory_plan,limb=limb)
 
-    return joint_plan
+    return joint_plan_list
     
